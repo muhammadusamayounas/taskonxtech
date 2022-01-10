67a0600b9937930fe8bae119142f4b56d127c5da
muhammadusamayounas <muhammad_osama@ucp.edu.pk>
2022-01-10 17:22:02 +0500

    Third Commit

--- resources/views/typeahead_autocomplete.blade.php
+++ resources/views/typeahead_autocomplete.blade.php
@@ -4,6 +4,12 @@
 <main class="sm:container sm:mx-auto sm:max-w-lg sm:mt-10">
 <html>
     <head>
+        <style>
+        .form-control-lg{
+            justify-content: center;
+            align-items: center;
+          }
+         </style>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
